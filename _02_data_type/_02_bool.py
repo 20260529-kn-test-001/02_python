31a6841bc64689d8cfb8dfa923520da43d526dd7
lululu9988 <konono.689@gmail.com>
2026-05-29 17:49:54 +0900

    20260529_데이터 타입(number,bool)

---
+++ _02_data_type/_02_bool.py
@@ -0,0 +1,26 @@
+# 논리형 boolean
+
+a=True
+b=False
+
+print(a,type(a))
+print(b,type(b))
+
+# 비교 연산
+# A>B: A가 B보다 크면 True/ 아니면 False
+# A>=B/복합 연산 기호에선 항상 등호가 오른쪽에 위치.
+
+# A==B: A=B이면 True/ 아니면 False
+# A!=B: A와 B가 서로 같지 않으면 True
+
+print("1 > 0.5:", 1 > 0.5)
+print("1 < 0.5:", 1 < 0.5)
+print("1 >= 0.5:", 1 >= 0.5)
+print("1 <= 0.5:", 1 <= 0.5)
+print("1 == 1:", 1 == 1)
+print("1 != 1:", 1 != 1)
+
+# 논리부정연산(not)
+print(not True)
+print(not False)
+print(not not True)
